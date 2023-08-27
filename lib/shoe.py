eaeb39e1c1c62e75fd5cb69cdda67bfd6d121cd9
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-08-28 00:07:53 +0300

    complete

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+
+    def cobble(self):
+        print(f"Your shoe is as good as new!")
+
+    def condition(self):
+        return "New"
+
+    def set_size(self, size):
+        if (type(size) is not int):
+            print("size must be an integer")
+        else:
+            self._size = size
+
+    def get_size(self):
+        return self._size
+    
+    size = property(get_size, set_size)
